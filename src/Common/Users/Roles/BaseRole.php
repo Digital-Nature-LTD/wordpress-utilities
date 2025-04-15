43c9905ccb395f9a7306cd66125c67c5c3cd13a6
Gareth Midwood <gareth@digital-nature.co.uk>
2025-04-15 10:35:32 +0100

    Ensure capabilities are added to role, even if new capabilities are added after the role is created. Also grant all capabilities to administrators

--- src/Common/Users/Roles/BaseRole.php
+++ src/Common/Users/Roles/BaseRole.php
@@ -27,12 +27,24 @@ abstract class BaseRole
      */
     public static function add_role(): void
     {
-        $capabilities = array_fill_keys(static::get_capabilities(), true);
-
+		// add the role with no capabilities
         add_role(
             static::get_role_slug(),
             static::get_role_name(),
-            $capabilities
+            []
         );
+
+		// get the role and add capabilities to ensure we're always up to date.
+	    // If we don't do this then the capabilities are only added when the role
+	    // is created, any capabilities added later will not be included.
+		$role = get_role(static::get_role_slug());
+	    $adminRole = get_role('administrator');
+
+	    $capabilities = array_fill_keys(static::get_capabilities(), true);
+
+		foreach ($capabilities as $capability => $granted) {
+			$role->add_cap($capability);
+			$adminRole->add_cap($capability);
+		}
     }
 }
